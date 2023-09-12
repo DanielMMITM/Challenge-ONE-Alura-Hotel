48dd4883ced5ad73f8ae34440eff646b47a96c79
Edgar Martínez <edgar.danmm@gmail.com>
2023-09-11 20:01:13 -0600

    Estructura de los modelos de acorde a la estructura declarada en la base de datos.

--- src/modelo/Huesped.java
+++ src/modelo/Huesped.java
@@ -1,4 +1,94 @@
 package modelo;
 
+import java.time.LocalDate;
+
 public class Huesped {
+    private Integer id;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String usuario;
+
+    private String contrasena;
+
+    private LocalDate fecha_de_nacimiento;
+
+    private String nacionalidad;
+
+    private String telefono;
+
+    public Huesped() {
+
+    }
+
+    public Huesped(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public LocalDate getFecha_de_nacimiento() {
+        return fecha_de_nacimiento;
+    }
+
+    public void setFecha_de_nacimiento(LocalDate fecha_de_nacimiento) {
+        this.fecha_de_nacimiento = fecha_de_nacimiento;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
--- src/modelo/Reserva.java
+++ src/modelo/Reserva.java
@@ -1,4 +1,71 @@
 package modelo;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Reserva {
+    private Integer id;
+
+    private LocalDateTime fecha_entrada;
+
+    private LocalDateTime fecha_salida;
+
+    private Integer valor;
+
+    private String forma_de_pago;
+
+    private Integer id_huesped;
+
+
+    public Reserva() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getFecha_entrada() {
+        return fecha_entrada;
+    }
+
+    public void setFecha_entrada(LocalDateTime fecha_entrada) {
+        this.fecha_entrada = fecha_entrada;
+    }
+
+    public LocalDateTime getFecha_salida() {
+        return fecha_salida;
+    }
+
+    public void setFecha_salida(LocalDateTime fecha_salida) {
+        this.fecha_salida = fecha_salida;
+    }
+
+    public Integer getValor() {
+        return valor;
+    }
+
+    public void setValor(Integer valor) {
+        this.valor = valor;
+    }
+
+    public String getForma_de_pago() {
+        return forma_de_pago;
+    }
+
+    public void setForma_de_pago(String forma_de_pago) {
+        this.forma_de_pago = forma_de_pago;
+    }
+
+    public Integer getId_huesped() {
+        return id_huesped;
+    }
+
+    public void setId_huesped(Integer id_huesped) {
+        this.id_huesped = id_huesped;
+    }
 }
